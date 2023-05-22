35f7780d25b210040642d07523a19dbe02615389
pWaw <przemko_wawrzyniak@wp.pl>
2023-05-22 09:34:43 +0200

    Initial commit

---
+++ src/main/java/pl/wawrzyniak/przemko/EasyIoT/EasyIoTApplication.java
@@ -0,0 +1,13 @@
+package pl.wawrzyniak.przemko.EasyIoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyIoTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wawrzyniak/przemko/EasyIoT/EasyIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wawrzyniak.przemko.EasyIoT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
